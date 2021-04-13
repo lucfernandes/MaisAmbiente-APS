6abfb8b57f0f6aa83f83f071d07dded9b67b9d0e
lukeeta <lucasfpf19@gmail.com>
2021-04-12 23:36:30 -0300

    Comentários

--- public/dist/js/index.js
+++ public/dist/js/index.js
@@ -1,20 +1,24 @@
 // Conexão com o Socket IO
 var socket = io('http://25.112.43.40:80');
 
+// Rendeniza as mensagens no chat
 function renderMessage(msg){
     $('.messages').append('<div class="msg"><b>'+ msg.author +'</b>: '+msg.message+'</div>');
 }
 
+// Recebe as mensagens anteriores que já haviam sido enviadas
 socket.on('previousMessage', function(msgs){
     for(msg of msgs){
         renderMessage(msg);
     }
 })
 
+// Recebe as mensagens que acabaram de ser enviadas
 socket.on('receivedMessage', function(msg){
     renderMessage(msg);
 })
 
+// Envia uma nova mensagem para o back-end
 $('#chat').submit(function(event){
     event.preventDefault();
 
--- server.js
+++ server.js
@@ -1,29 +1,44 @@
+// Importa o Express e o Path para aplicação
 const express = require('express');
 const path = require('path');
 
+// Instância o Express
 const app = express();
+
+// Importa HTTP e cria o servidor
 const server = require('http').createServer(app);
+
+// Instância o Socket.IO no servidor criado
 const io = require('socket.io')(server);
 
+// Registra o host e a porta que será hospedada a aplicação
 const hostname = '25.112.43.40';
 const port = 80;
 
+// Set de onde estará a aplicação base
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname,'public'));
+
+// Set da tecnologia utilizada para o front
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// Cria a rota que será utilizada para acessar a aplicação
 app.use('/', (req, res)=>{
     res.render('index.html')
 })
 
+// Array que guardará as mensagens do chat
 let messages = [];
 
+// Inicia conexão com o Socket
 io.on('connection', socket => {
     console.log('Socket Conectado:'+socket.id);
 
+    // Envia as mensagens previamente registradas para o cliente
     socket.emit('previousMessage', messages);
 
+    // Recebe as novas mensagens do cliente e repassa para os outros clientes.
     socket.on('sendMessage', data => {
         messages.push(data);
         console.log(data);
@@ -31,6 +46,7 @@ io.on('connection', socket => {
     })
 })
 
+// Hospeda a aplicação na porta e no host indicado
 server.listen(port, hostname, ()=>{
     console.log(`Server running at http://${hostname}:${port}/`)
 });
